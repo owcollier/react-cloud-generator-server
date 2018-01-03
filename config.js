d87f3cf874a7436ebfd8c8abba8341b885988ed4
Owcollier <owcollier@gmail.com>
2018-01-03 16:35:00 -0500

    updated config to work with mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-    DATABASE_URL: 'mongodb:dev:dev@ds237967.mlab.com:37967/wordcloud-library'
+    DATABASE_URL: 'mongodb://dev:dev@ds237967.mlab.com:37967/wordcloud-library'
     // 'mongodb://localhost/wordcloud-library'
 };
